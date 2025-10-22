062b2eb20c6b6fa096479b53486fa8405aed5387
Hook-88 <82540853+Hook-88@users.noreply.github.com>
2025-10-22 13:59:57 -0500

    add onmounted hook

--- src/.vuepress/layouts/Layout.vue
+++ src/.vuepress/layouts/Layout.vue
@@ -1,6 +1,21 @@
 <script setup>
 import { Layout } from "vuepress-theme-hope/client"
 import ArticleFooter from "../components/ArticleFooter.vue";
+import { onMounted } from "vue";
+
+
+onMounted(() => {
+    if (window.netlifyIdentity) {
+        window.netlifyIdentity.on("init", (user) => {
+            if (!user) {
+                window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/"
+                })
+            }
+        })
+    }
+})
+
 </script>
 
 <template>
